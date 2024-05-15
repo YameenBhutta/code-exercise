3b6605136b301e9196c91f5ce610c756e8780454
“Yameen <yameenbhutta001@gmail.com>
2024-05-15 16:56:48 +0500

    Set up project structure, installed Tailwind CSS, and added layout component

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{env('APP_NAME')}}</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+<header class="py-8 bg-slate-800">
+<div class="container mx-auto flex justify-between items-center px-6">
+
+<h1 class="text-2xl font-semibold text-white">Code Exercise</h1>
+<nav>
+<a href="#" class="text-white font-normal text-xl mr-4">Login</a>
+<a href="#" class="text-white font-normal text-xl">Sign up</a>
+</nav>
+
+</div>
+</header>    
+</body>
+</html>
